ff7097d75cd4e981a5328c1a299a673dddf711ee
Kelven Bragoa <kelvenbragoa@hotmail.com>
2025-02-06 20:19:38 +0200

    first coomiit

--- app/Http/Controllers/Api/Web/ProductController.php
+++ app/Http/Controllers/Api/Web/ProductController.php
@@ -54,7 +54,7 @@ class ProductController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        ////
         // $data = $request->all();
         // $imageName = null;
         // if($request->has('image')){
